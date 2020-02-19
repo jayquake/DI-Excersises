bada1c26682d525cb81701a8885b5c8df0c1c3c2
Jason <jayquake@gmail.com>
2020-02-19 16:37:29 +0200

    week4

--- week4/Caesar Cypher.py
+++ week4/Caesar Cypher.py
@@ -1,10 +1,47 @@
 import abc
 alphabet = "a"
 lists = []
+lists2 = []
+string_to_change = input("imput what you want encrypted")
+splice_Legnth = input("enter a number")
 for i in range(0,26):
     lists.append(alphabet)
     alphabet = chr(ord(alphabet)+1)
-    # print(lists[0])
-    for a in alphabet:
-        "a"== "d"
-        print(alphabet)
+print(lists*2)
+
+
+def encrypt(text, splice_Legnth):
+    lists*2
+    for i in range(len(text)):
+        for j in range(len(lists)):
+            if lists*2 == list:
+                text ==
+
+
+
+# for i in range(3,26):
+#     lists2.append(alphabet)
+#     alphabet = chr(ord(alphabet)+1)
+# print(lists2)
+
+# num = 1
+# lists= []
+# for i in range((chr(ord(num)+1):
+#     lists.append(num)
+# print(lists)
+
+#
+# def switch_letters():
+#     dbet = ['d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z', 'a', 'b', 'c']
+#     letters = input("enter a few letters ")
+#     if letters == lists[0]:
+#         encrypt = [i]+3
+#     return letters
+#
+# print(switch_letters())
+
+# letters = ['a','b','c','d','d','m','n','n']
+#
+# def find_duplicates(letters):
+#     list.count(letters)
+#     if letters
